8d556c4623566f0b5a4d26e736a800c5fd44771a
Shaquille Walton <itscoldinwinter2@gmail.com>
2021-03-18 18:43:30 -0700

    stuff

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 // BUILD YOUR SERVER HERE
-const Users = require('./users/model.js');
+const users = require('./users/model.js');
 
 const express = require('express');
 
@@ -7,12 +7,43 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/api/users', (request, response) => {
+server.get('/', (request, response) => {
     try{
-        response.status(200).json(users);
+        response.status(200).json(users.find());
     }catch{
         response.status(500).json({ errorMessage: "The user information could not be retrieved."})
     }
     
 })
+
+//create
+server.post('/api/users', async  (request, response) =>{
+    const user = request.body;
+
+    if(!user.name|| !user.bio){
+        response.status(400).json({message: "User must include name and bio"});
+    }
+    else{
+        try{
+            const newUser = await users.create(user);
+            response.status(200).json(newUser);
+        }
+        catch (err){
+            response.status(500).json({error: err.message});
+        }
+    }
+})
+
+//read
+server.get('/api/users', async (request, response) => {
+    try{
+        const user = await users.find();
+        response.status(200).json(user);
+    }
+    catch (err) {
+        response.status(500).json({error: err.message});
+    }
+})
+
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
